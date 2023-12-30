d3538d723b757fa11617f986ba96f1c38989895d
NirajMourya <nirajmourya786@gmail.com>
2023-12-30 21:35:06 +0530

    Mod Player cmp /added Timer Chlng cmp

--- ticktockchallenge/src/components/Player.jsx
+++ ticktockchallenge/src/components/Player.jsx
@@ -1,10 +1,21 @@
+import { useState,useRef } from "react";
+
 export default function Player() {
+  const PlayerNameInput = useRef();
+  const [PlayerName,setPlayerName] = useState(null);
+ 
+  function handleClick()
+  {
+      setPlayerName(PlayerNameInput.current.value);
+      PlayerNameInput.current.value = '';
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome { PlayerName ?? 'unknown entity' }</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input ref={PlayerNameInput} type="text" />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
